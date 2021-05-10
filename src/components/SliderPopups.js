fe05aa1edd4f7ecd6b2312d6a3952ae06a95f858
nackalalalong <nak-online@hotmail.com>
2021-05-10 18:30:45 +0700

    fix responsive

--- src/components/SliderPopups.js
+++ src/components/SliderPopups.js
@@ -3,19 +3,30 @@ import React, { useState } from 'react'
 import './map.css'
 import { Popup } from 'react-mapbox-gl'
 import { coordinates } from './data'
+import useWindowDimensions from './UseWindowDimensions'
 
 const SliderPopups = (props) => {
 
+  const SliderWithTooltip = Slider.createSliderWithTooltip(Slider)
+
   const [sliderVal, setSlideVal] = useState(1)
+  const { height, width } = useWindowDimensions();
 
   const handleAfterChange = e => {
     setSlideVal(e)
   }
 
   const marks = {};
-  [...Array(72).keys()].forEach(e => {
-    marks[e + 1] = { style: '', label: e + 1 }
-  });
+  if ( width <= 1600 ){
+    [...Array(18).keys()].forEach(e => {
+      marks[(e + 1)*4] = { style: '', label: (e + 1)*4 }
+    });
+  }
+  else {
+    [...Array(72).keys()].forEach(e => {
+      marks[e + 1] = { style: '', label: e + 1 }
+    });
+  }
 
   const popups = Object.keys(coordinates).map(province => (
     <Popup 
@@ -34,7 +45,7 @@ const SliderPopups = (props) => {
     <>
       {popups}
       <div className="slider-holder">
-        <Slider min={1} max={72} onAfterChange={handleAfterChange} defaultValue={sliderVal} marks={marks} />
+        <SliderWithTooltip min={1} max={72} onAfterChange={handleAfterChange} defaultValue={sliderVal} marks={marks} />
       </div>
       <p className="slider-desc-text">next hours to predict</p>
     </>
